b6eadcffa52a81831d755862234899fd4ff9aa39
KeeganMauger <keeganmauger@cmail.carleton.ca>
2022-01-28 15:35:55 -0500

    Bounding rectangle

---
+++ MCMETmain.m
@@ -0,0 +1,12 @@
+% ELEC 4700
+% Assignment 1: Monte-Carlo Modeling of Electron Transport
+% Keegan Mauger
+
+% Bounding Area: 200nm x 100nm
+
+% rectangle('Position',[1 2 5 6])
+% axis([0 10 0 10])
+
+run WC
+
+rectangle('Position',[0 0 200e-9 100e-9])
