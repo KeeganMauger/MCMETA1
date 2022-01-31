2ec630104c0ce17db7dca6afc97dff6b5a7b1f5b
KeeganMauger <keeganmauger@cmail.carleton.ca>
2022-01-31 16:18:29 -0500

    Starting progress

--- PlotVars.m
+++ PlotVars.m
@@ -30,66 +30,69 @@ if c==0 && ~ScaleV
     ScaleV = sqrt(dx*dx + dy*dy) / MaxV * 0.15;
 end
 
-subplot(3, 2, 1), plot(x(Pty0in), y(Pty0in), 'bo', 'markers',...
-    MarkerSize,'MarkerFaceColor', 'b');
-hold on
-subplot(3, 2, 1), plot(x(Pty1in), y(Pty1in), 'go', 'markers',...
-    MarkerSize,'MarkerFaceColor', 'g');
-subplot(3, 2, 1),quiver(x, y, Fx * ScaleF, Fy * ScaleF, 0, 'm', 'linewidth', 2);
-hold off
-axis(Limits);
-title('Atoms and Forces')
-xlabel('X')
-ylabel('Y')
+% subplot(3, 2, 1), plot(x(Pty0in), y(Pty0in), 'bo', 'markers',...
+%     MarkerSize,'MarkerFaceColor', 'b');
+% hold on
+% subplot(3, 2, 1), plot(x(Pty1in), y(Pty1in), 'go', 'markers',...
+%     MarkerSize,'MarkerFaceColor', 'g');
+% subplot(3, 2, 1),quiver(x, y, Fx * ScaleF, Fy * ScaleF, 0, 'm', 'linewidth', 2);
+% hold off
+% axis(Limits);
+% title('Atoms and Forces')
+% xlabel('X')
+% ylabel('Y')
 
-subplot(3, 2, 2), plot(time, KETot, 'linewidth', 2);
-hold on
-subplot(3, 2, 2), plot(time, PhiTot + LJEpsilon * nAtoms, 'g', 'linewidth',2);
-subplot(3, 2, 2), plot(time, PhiTot + KETot + LJEpsilon * nAtoms, 'k', 'linewidth',2);
-%     axis([0 TStop 0e-22 3e-23]);
-xlabel('time')
-ylabel('Energy')
-title('Total, Kinetic and Potential Energy')
+% subplot(3, 2, 2), plot(time, KETot, 'linewidth', 2);
+% hold on
+% subplot(3, 2, 2), plot(time, PhiTot + LJEpsilon * nAtoms, 'g', 'linewidth',2);
+% subplot(3, 2, 2), plot(time, PhiTot + KETot + LJEpsilon * nAtoms, 'k', 'linewidth',2);
+% %     axis([0 TStop 0e-22 3e-23]);
+% xlabel('time')
+% ylabel('Energy')
+% title('Total, Kinetic and Potential Energy')
 
-subplot(3,2,3), plot(x(Pty0in), y(Pty0in), 'bo', 'markers',...
+% subplot(3,2,3), 
+    plot(x(Pty0in), y(Pty0in), 'bo', 'markers',...
     MarkerSize,'MarkerFaceColor','b');
 hold on
-subplot(3,2,3), plot(x(Pty1in), y(Pty1in), 'go', 'markers',...
+% subplot(3,2,3), 
+    plot(x(Pty1in), y(Pty1in), 'go', 'markers',...
     MarkerSize,'MarkerFaceColor','g');
-subplot(3,2,3),quiver(x, y, Vx * ScaleV, Vy * ScaleV, 0, 'r', 'linewidth', 2);
+% subplot(3,2,3),
+quiver(x, y, Vx * ScaleV, Vy * ScaleV, 0, 'r', 'linewidth', 2);
 hold off
 axis(Limits);
 title('Atoms and Velocities')
 xlabel('X')
 ylabel('Y')
 
-AE(Pty0in) = 1 / 2 * Mass0 * V2(Pty0in) + Phi(Pty0in) - Phi0;
-AE(Pty1in) = 1 / 2 * Mass1 * V2(Pty1in) + Phi(Pty1in) - Phi0;
-% if AddParticle, AE(end) = 0;end
-% AE = Phi;
-subplot(3, 2, 4),scatter3(x,y,AE,ones(1,nAtoms)*300,AE,'fill');
-axis(Limits);
-view(2);
-title('Atom Energy (KE + Pot)')
+% AE(Pty0in) = 1 / 2 * Mass0 * V2(Pty0in) + Phi(Pty0in) - Phi0;
+% AE(Pty1in) = 1 / 2 * Mass1 * V2(Pty1in) + Phi(Pty1in) - Phi0;
+% % if AddParticle, AE(end) = 0;end
+% % AE = Phi;
+% subplot(3, 2, 4),scatter3(x,y,AE,ones(1,nAtoms)*300,AE,'fill');
+% axis(Limits);
+% view(2);
+% title('Atom Energy (KE + Pot)')
 
-subplot(3, 2, 5), plot(x(Pty0in), y(Pty0in), 'bo', 'markers',...
-    MarkerSize, 'MarkerFaceColor', 'b');
-hold on
-subplot(3, 2, 5), plot(x(Pty1in), y(Pty1in), 'go', 'markers',...
-    MarkerSize, 'MarkerFaceColor', 'g');
-hold off
-title('Atoms (All)')
-xlabel('X')
-ylabel('Y')
+% subplot(3, 2, 5), plot(x(Pty0in), y(Pty0in), 'bo', 'markers',...
+%     MarkerSize, 'MarkerFaceColor', 'b');
+% hold on
+% subplot(3, 2, 5), plot(x(Pty1in), y(Pty1in), 'go', 'markers',...
+%     MarkerSize, 'MarkerFaceColor', 'g');
+% hold off
+% title('Atoms (All)')
+% xlabel('X')
+% ylabel('Y')
 
-subplot(3, 2, 6), plot(time, T, 'k', 'linewidth', 2);
-hold on
-subplot(3, 2, 6), plot(time, T0, 'b', 'linewidth', 2);
-subplot(3, 2, 6), plot(time, T1, 'g', 'linewidth', 2);
-%
-xlabel('time')
-ylabel('Temperature')
-title('Temperature: Each type and all')
+% subplot(3, 2, 6), plot(time, T, 'k', 'linewidth', 2);
+% hold on
+% subplot(3, 2, 6), plot(time, T0, 'b', 'linewidth', 2);
+% subplot(3, 2, 6), plot(time, T1, 'g', 'linewidth', 2);
+% %
+% xlabel('time')
+% ylabel('Temperature')
+% title('Temperature: Each type and all')
 
 % if doPlotImage
 %     if PlotCount == 1
